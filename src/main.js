84a281cac6be89c76cc0a17058853fbd77899d87
patrickcmd <pwalukagga@gmail.com>
2023-02-10 14:23:43 +0300

    Write to celo contract

--- src/main.js
+++ src/main.js
@@ -1,16 +1,35 @@
 import Web3 from 'web3';
 import { newKitFromWeb3 } from '@celo/contractkit';
 import BigNumber from 'bignumber.js';
+import movieLibraryAbi from '../contract/movie_library.abi.json';
 
 const ERC20_DECIMALS = 18;
+const MPContractAddress = "0x61436575Fc27bbEf8414198EeD91348593BeAF21"
 
 let kit;
+let contract;
+/*
+{
+    title: "Spider-Man 3",
+    genres: `Action|Adventure|Romance`,
+    image: "https://rukminim1.flixcart.com/image/832/832/kbgu1e80/physical-game/v/j/u/spider-man-game-spider-man-3-action-adventure-fighting-shooting-original-imafssfnxtfgtfcn.jpeg?q=70",
+    imbd_link: "http://www.imdb.com/title/tt0413300/?ref_=fn_tt_tt_1",
+    content_rating: "PG-13",
+    owner: "0x32Be343B94f860124dC4fEe278FDCBD38C102D88",
+    year: 2007,
+    imbd_score: 7,
+    view_price: 3,
+    views: 27,
+    user_reviews: 1903,
+    index: 0,
+}
+*/
+let movies = [];
 
 const connectCeloWallet = async function () {
     if (window.celo) {
-        notification("⚠️ Please approve this DApp to use it.")
-
         try {
+            notification("⚠️ Please approve this DApp to use it.")
             await window.celo.enable()
             notificationOff()
 
@@ -19,6 +38,8 @@ const connectCeloWallet = async function () {
 
             const accounts = await kit.web3.eth.getAccounts()
             kit.defaultAccount = accounts[0]
+
+            contract = new kit.web3.eth.Contract(movieLibraryAbi, MPContractAddress)
         } catch (error) {
             notification(`⚠️ ${error}.`)
         }
@@ -26,95 +47,84 @@ const connectCeloWallet = async function () {
         notification("⚠️ Please install the CeloExtensionWallet.")
     }
 }
-
-const movies = [
-    {
-      title: "Spider-Man 3",
-      genres: `Action|Adventure|Romance`,
-      image: "https://rukminim1.flixcart.com/image/832/832/kbgu1e80/physical-game/v/j/u/spider-man-game-spider-man-3-action-adventure-fighting-shooting-original-imafssfnxtfgtfcn.jpeg?q=70",
-      imbd_link: "http://www.imdb.com/title/tt0413300/?ref_=fn_tt_tt_1",
-      content_rating: "PG-13",
-      owner: "0x32Be343B94f860124dC4fEe278FDCBD38C102D88",
-      year: 2007,
-      imbd_score: 7,
-      view_price: 3,
-      views: 27,
-      user_reviews: 1903,
-      index: 0,
-    },
-    {
-        title: "Avengers: Age of Ultron",
-        genres: `Action|Adventure|Sci-Fi`,
-        image: "https://pbs.twimg.com/media/B-n_ClcUAAAMdpM?format=jpg&name=large",
-        imbd_link: "http://www.imdb.com/title/tt2395427/?ref_=fn_tt_tt_1",
-        content_rating: "PG-13",
-        owner: "0x3275B7F400cCdeBeDaf0D8A9a7C8C1aBE2d747Ea",
-        year: 2015,
-        imbd_score: 8,
-        view_price: 4,
-        views: 27,
-        user_reviews: 1117,
-        index: 1,
-    },
-    {
-        title: "Harry Potter and the Half-Blood Prince",
-        genres: `Adventure|Family|Fantasy|Mystery`,
-        image: "https://darkroom.bbfc.co.uk/751/https://m.media-amazon.com/images/M/MV5BNzU3NDg4NTAyNV5BMl5BanBnXkFtZTcwOTg2ODg1Mg@@._V1_.jpg:e078e76491dceb4d951c0fb2bd7c61cd",
-        imbd_link: "http://www.imdb.com/title/tt0417741/?ref_=fn_tt_tt_1",
-        content_rating: "PG",
-        owner: "0x2EF48F32eB0AEB90778A2170a0558A941b72BFFb",
-        year: 2009,
-        imbd_score: 8,
-        view_price: 3,
-        views: 30,
-        user_reviews: 973,
-        index: 2,
-    },
-    {
-        title: "Men in Black 3",
-        genres: `Action|Adventure|Comedy|Family|Fantasy|Sci-Fi`,
-        image: "https://darkroom.bbfc.co.uk/751/https://m.media-amazon.com/images/M/MV5BMTU2NTYxODcwMF5BMl5BanBnXkFtZTcwNDk1NDY0Nw@@._V1_.jpg:3f0ebb6cef594b60d28f64a06ab5f5fb",
-        imbd_link: "http://www.imdb.com/title/tt1409024/?ref_=fn_tt_tt_1",
-        content_rating: "PG-13",
-        owner: "0x2EF48F32eB0AEB90778A2170a0558A941b72BFFb",
-        year: 2012,
-        imbd_score: 7,
-        view_price: 4,
-        views: 17,
-        user_reviews: 341,
-        index: 3,
-    },
-  ]
   
-  const getBalance = async function () {
+const getBalance = async function () {
     const totalBalance = await kit.getTotalBalance(kit.defaultAccount)
     const cUSDBalance = totalBalance.cUSD.shiftedBy(-ERC20_DECIMALS).toFixed(2)
     document.querySelector("#balance").textContent = cUSDBalance
-  }
-  
-  function renderMovies() {
+}
+
+const getMovieContentRating = async function(index) {
+    const rating = await contract.methods.getMovieContentRating(index).call()
+    return rating
+}
+
+const getMovieUserReviews = async function(index) {
+    const user_reviews = await contract.methods.getMovieUserReviews(index).call()
+    return user_reviews
+}
+
+const getMovieImbdScore = async function(index) {
+    const imbd_score = await contract.methods.getMovieImbdScore(index).call()
+    return imbd_score
+}
+
+const getMovieImbdLink = async function(index) {
+    const imbd_link = await contract.methods.getMovieImbdLink(index).call()
+    return imbd_link
+}
+
+const getMovies = async function () {
+    const _moviesLength = await contract.methods.getMoviesLength().call()
+    const _movies = []
+
+    for (let i = 0; i < _moviesLength; i++) {
+        let _movie = new Promise(async (resolve, reject) => {
+            let m = await contract.methods.readMovie(i).call()
+            resolve({
+                index: i,
+                owner: m[0],
+                title: m[1],
+                genres: m[2],
+                image: m[3],
+                year: m[4],
+                views: m[5],
+                view_price: m[6],
+                content_rating: await getMovieContentRating(i),
+                user_reviews: await getMovieUserReviews(i),
+                imbd_score: await getMovieImbdScore(i),
+                imbd_link: await getMovieImbdLink(i)
+            })
+        })
+        _movies.push(_movie)
+    }
+    movies = await Promise.all(_movies)
+    renderMovies()
+}
+
+function renderMovies() {
     document.getElementById("movielibrary").innerHTML = ""
     movies.forEach((_movie) => {
-      const newDiv = document.createElement("div")
-      newDiv.className = "col-md-4"
-      newDiv.innerHTML = movieTemplate(_movie)
-      document.getElementById("movielibrary").appendChild(newDiv)
+        const newDiv = document.createElement("div")
+        newDiv.className = "col-md-4"
+        newDiv.innerHTML = movieTemplate(_movie)
+        document.getElementById("movielibrary").appendChild(newDiv)
     })
-  }
-  
-  function movieTemplate(_movie) {
+}
+
+function movieTemplate(_movie) {
     return `
-      <div class="card mb-4">
+        <div class="card mb-4">
         <img class="card-img-top" src="${_movie.image}" alt="...">
         <div class="position-absolute top-0 end-0 bg-warning mt-4 px-2 py-1 rounded-start">
-          ${_movie.views} Viewed
+            ${_movie.views} Viewed
         </div>
         <div class="card-body text-left p-4 position-relative">
-          <div class="translate-middle-y position-absolute top-0">
-          ${identiconTemplate(_movie.owner)}
-          </div>
-          <h2 class="card-title fs-4 fw-bold mt-2">${_movie.title}</h2>
-          <p class="card-text mb-4" style="min-height: 82px">
+            <div class="translate-middle-y position-absolute top-0">
+            ${identiconTemplate(_movie.owner)}
+            </div>
+            <h2 class="card-title fs-4 fw-bold mt-2">${_movie.title}</h2>
+            <p class="card-text mb-4" style="min-height: 82px">
             <strong>Move IMBD Score</strong>: ${_movie.imbd_score}
             <br>
             <strong>Year released</strong>: ${_movie.year}
@@ -126,86 +136,92 @@ const movies = [
             Follow movie IMBD link to see movie trailor: 
             <a target="_blank" href="${_movie.imbd_link}">IMBD Link</a> 
             <br>
-          </p>
-          <p class="card-text mt-4">
+            </p>
+            <p class="card-text mt-4">
             <strong>Genres</strong>:
             <span>${_movie.genres}</span>
-          </p>
-          <div class="d-grid gap-2">
+            </p>
+            <div class="d-grid gap-2">
             <a class="btn btn-lg btn-outline-dark viewBtn fs-6 p-3" id=${
-              _movie.index
+                _movie.index
             }>
-              View for ${_movie.view_price} cUSD
+                View for ${_movie.view_price} cUSD
             </a>
-          </div>
+            </div>
         </div>
-      </div>
+        </div>
     `
-  }
-  
-  function identiconTemplate(_address) {
+}
+
+function identiconTemplate(_address) {
     const icon = blockies
-      .create({
+        .create({
         seed: _address,
         size: 8,
         scale: 16,
-      })
-      .toDataURL()
-  
+        })
+        .toDataURL()
+
     return `
-    <div class="rounded-circle overflow-hidden d-inline-block border border-white border-2 shadow-sm m-0">
-      <a href="https://alfajores-blockscout.celo-testnet.org/address/${_address}/transactions"
-          target="_blank">
-          <img src="${icon}" width="48" alt="${_address}">
-      </a>
-    </div>
+        <div class="rounded-circle overflow-hidden d-inline-block border border-white border-2 shadow-sm m-0">
+            <a href="https://alfajores-blockscout.celo-testnet.org/address/${_address}/transactions"
+                target="_blank">
+                <img src="${icon}" width="48" alt="${_address}">
+            </a>
+        </div>
     `
-  }
-  
-  function notification(_text) {
+}
+
+function notification(_text) {
     document.querySelector(".alert").style.display = "block"
     document.querySelector("#notification").textContent = _text
-  }
-  
-  function notificationOff() {
+}
+
+function notificationOff() {
     document.querySelector(".alert").style.display = "none"
-  }
-  
-  window.addEventListener("load", async () => {
+}
+
+window.addEventListener("load", async () => {
     notification("⌛ Loading...")
     await connectCeloWallet()
     await getBalance()
-    renderMovies()
+    await getMovies()
     notificationOff()
-  })
-  
-  document
+})
+
+document
     .querySelector("#newMovieBtn")
-    .addEventListener("click", () => {
-      const _movie = {
-        owner: "0x2EF48F32eB0AEB90778A2170a0558A941b72BFFb",
-        title: document.getElementById("newMovieTitle").value,
-        genres: document.getElementById("newMovieGenres").value,
-        image: document.getElementById("newImgUrl").value,
-        imbd_link: document.getElementById("newImbdLink").value,
-        content_rating: document.getElementById("newContentRating").value,
-        imbd_score: document.getElementById("newMovieImbdScore").value,
-        year: document.getElementById("newMovieYear").value,
-        user_reviews: document.getElementById("newMovieUserReviews").value,
-        view_price: document.getElementById("newMovieViewPrice").value,
-        views: 0,
-        index: movies.length,
-      }
-      movies.push(_movie)
-      notification(`🎉 You successfully added "${_movie.title}".`)
-      renderMovies()
+    .addEventListener("click", async (e) => {
+        const params = [
+            document.getElementById("newMovieTitle").value,
+            document.getElementById("newMovieGenres").value,
+            document.getElementById("newImgUrl").value,
+            document.getElementById("newImbdLink").value,
+            document.getElementById("newContentRating").value,
+            document.getElementById("newMovieYear").value,
+            document.getElementById("newMovieImbdScore").value,
+            document.getElementById("newMovieUserReviews").value,
+            new BigNumber(document.getElementById("newMovieViewPrice").value)
+            .shiftedBy(ERC20_DECIMALS)
+            .toString()
+        ]
+        notification(`⌛ Adding "${params[0]}"...`)
+        try {
+            const result = await contract.methods
+            .writeMovie(...params)
+            .send({ from: kit.defaultAccount })
+        } catch (error) {
+            notification(`⚠️ ${error}.`)
+        }
+        notification(`🎉 You successfully added "${params[0]}".`)
+        getMovies()
     })
-  
-  document.querySelector("#movielibrary").addEventListener("click", (e) => {
+
+document.querySelector("#movielibrary").addEventListener("click", (e) => {
     if(e.target.className.includes("viewBtn")) {
-      const index = e.target.id
-      movies[index].sold++
-      notification(`🎉 You successfully viewed "${movies[index].title}".`)
-      renderMovies()
+        const index = e.target.id
+        movies[index].views++
+        notification(`🎉 You successfully viewed "${movies[index].title}".`)
+        renderMovies()
     }
-  })
+})
