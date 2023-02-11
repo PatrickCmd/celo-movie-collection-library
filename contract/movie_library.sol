ff08c0380b22d6d579bddf35991855274abd71a7
sam-the-tutor <atwebembeiresamuel2020@gmail.com>
2023-02-12 02:17:35 +0300

    Error Fixes

--- contract/movie_library.sol
+++ contract/movie_library.sol
@@ -4,37 +4,31 @@ pragma solidity >=0.7.0 <0.9.0;
 
 interface IERC20Token {
   function transfer(address, uint256) external returns (bool);
+
   function approve(address, uint256) external returns (bool);
+
   function transferFrom(address, address, uint256) external returns (bool);
+
   function totalSupply() external view returns (uint256);
+
   function balanceOf(address) external view returns (uint256);
+
   function allowance(address, address) external view returns (uint256);
 
   event Transfer(address indexed from, address indexed to, uint256 value);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 }
 
-/* Movie Database
-owner
-title
-genres
-actors
-image
-movie_imbd_link
-imbd_score
-content_rating
-year
-user_reviews
-views
-view_price
-country
-*/
 
+//Contract store movie details uploaded by users
 contract MovieLibrary {
-
+    //track the movie count
     uint internal moviesLength = 0;
+
+    //cUSd token contract address
     address internal cUsdTokenAddress = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
 
+    //Organize movie details in a struct
     struct Movie {
         address payable owner;
         string title;
@@ -49,27 +43,43 @@ contract MovieLibrary {
         uint view_price;
     }
 
-    mapping (uint => Movie) internal movies;
+    //track all movies
+    mapping (uint => Movie) public movies;
 
+
+    //track movies bought by a specific address
+    mapping(address => Movie[]) public myPaidMovies;
+
+
+    //store movie details in the  smart contract
     function writeMovie(
-        string memory _title,
-        string memory _genres,
-        string memory _image,
-        string memory _movie_imbd_link,
-        string memory _content_rating,
+        string calldata _title,
+        string calldata _genres,
+        string calldata _image,
+        string calldata _imbdLink,
+        string calldata _contentRating,
         uint _year,
         uint _imbd_score,
         uint _user_reviews,
         uint _view_price
+
     ) public {
+
+        require(bytes(_title).length > 0, "Input is invalid");
+        require(bytes(_genres).length > 0, "Input is invalid");
+        require(bytes(_image).length > 0, "Input is invalid");
+        require(bytes(_imbdLink).length > 0, "Input is invalid");
+        require(bytes(_contentRating).length > 0, "Input is invalid");
+        require(_view_price > 0, "Input is invalid");
+
         uint _views = 0;
         movies[moviesLength] = Movie(
             payable(msg.sender),
             _title,
             _genres,
             _image,
-            _movie_imbd_link,
-            _content_rating,
+            _imbdLink,
+            _contentRating,
             _year,
             _imbd_score,
             _user_reviews,
@@ -79,26 +89,14 @@ contract MovieLibrary {
         moviesLength++;
     }
 
-    function readMovie(uint _index) public view returns (
-        address payable,
-        string memory,
-        string memory,
-        string memory,
-        uint,
-        uint,
-        uint
-    ) {
-        return (
-            movies[_index].owner,
-            movies[_index].title,
-            movies[_index].genres,
-            movies[_index].image,
-            movies[_index].year,
-            movies[_index].views,
-            movies[_index].view_price
-        );
+    //return movie with a specific index
+    function readMovie(uint _index) public view returns (Movie memory) {
+        return  movies[_index];
+    
     }
 
+
+    //pay for a movie to watch it
     function viewMovie(uint _index) public payable  {
         require(
           IERC20Token(cUsdTokenAddress).transferFrom(
@@ -109,25 +107,22 @@ contract MovieLibrary {
           "Transfer failed."
         );
         movies[_index].views++;
+        myPaidMovies[msg.sender].push(movies[_index]);
     }
 
-    function getMoviesLength() public view returns (uint) {
-        return (moviesLength);
+
+    //get all movies paid by the user
+    function getMyPaidMovies() public view returns(Movie[] memory){
+        return myPaidMovies[msg.sender];
     }
 
-    function getMovieUserReviews(uint _index) public view returns (uint) {
-        return (movies[_index].user_reviews);
-    }
 
-    function getMovieImbdScore(uint _index) public view returns (uint) {
-        return (movies[_index].imbd_score);
-    }
-
-    function getMovieContentRating(uint _index) public view returns (string memory) {
-        return (movies[_index].content_rating);
-    }
-
-    function getMovieImbdLink(uint _index) public view returns (string memory) {
-        return (movies[_index].movie_imbd_link);
-    }
+//Delete a movie
+function deleteMovie(uint _index) public{
+    require(movies[_index].owner == msg.sender,"You are not authorized");
+    delete movies[_index];
 }
+
+
+}
+
